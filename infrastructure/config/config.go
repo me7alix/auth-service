3678741be3fce2ec7d567d214581f8a8dd20d8fc
me7alix <me7alix@gmail.com>
2026-09-10 17:54:29 +0300

    update config

--- infrastructure/config/config.go
+++ infrastructure/config/config.go
@@ -1,17 +1,42 @@
 package config
 
-import "os"
+import (
+	"os"
+	"fmt"
+	"strconv"
+)
 
 type Config struct {
-	DatabaseURL 	string
-	RedisURL 		string
-	JWTSecret 		string
+	LogLevel    string
+	DatabaseURL string
+	RedisURL    string
+	JWTSecret   string
+	Port        int
+}
+
+func getEnvStr(key string) string {
+	res := os.Getenv(key)
+	if res == "" {
+		panic(fmt.Errorf("%s is not provided", key))
+	}
+	return res
+}
+
+func getEnvInt(key string) int {
+	str := getEnvStr(key)
+	res, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		panic(fmt.Errorf("%s parsing error", key))
+	}
+	return int(res)
 }
 
 func LoadConfig() *Config {
 	return &Config{
-		DatabaseURL: 	os.Getenv("DATABASE_URL"),
-		RedisURL: 		os.Getenv("REDIS_URL"),
-		JWTSecret: 		os.Getenv("JWT_SECRET"),
+		LogLevel:    getEnvStr("LOG_LEVEL"),
+		DatabaseURL: getEnvStr("DATABASE_URL"),
+		RedisURL:    getEnvStr("REDIS_URL"),
+		JWTSecret:   getEnvStr("JWT_SECRET"),
+		Port:        getEnvInt("PORT"),
 	}
 }

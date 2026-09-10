6ed04d35b11e0f90d9b7b4268ae89b0adfbeac2f
me7alix <me7alix@gmail.com>
2026-09-10 17:56:11 +0300

    new logger

--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/phuslu/log v1.0.120 // indirect
 	github.com/redis/go-redis/v9 v9.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,16 +6,29 @@ import (
 	"user-service/infrastructure/config"
 	"user-service/infrastructure/repository"
 	"user-service/userinterface"
+	"github.com/phuslu/log"
 )
 
 func main() {
 	config := config.LoadConfig()
+	logLevel := log.ParseLevel(config.LogLevel)
+
+	log.DefaultLogger = log.Logger{
+		TimeFormat: "15:04:05",
+		Level: logLevel,
+		Caller: 1,
+		Writer: &log.ConsoleWriter{
+			ColorOutput: true,
+			QuoteString: true,
+			EndWithMessage: true,
+		},
+	}
 
 	userRep := repository.NewUserRepository(config.DatabaseURL)
 	userCache := cache.NewUserCache(config.RedisURL)
 
 	authService := service.NewAuthService(userRep, userCache, config.JWTSecret)
-	userService := service.NewUserService(userRep)
+	userService := service.NewUserService(userRep, userCache)
 
-	api.Run(userService, authService)
+	api.Run(config.Port, userService, authService)
 }
